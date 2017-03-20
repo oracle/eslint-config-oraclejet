183c78b38f4d1ff7616b3020b98319f2f1375359
blm7 <ben.moroze@oracle.com>
2017-03-20 08:02:43 -0700

    Release 3.0.0

---
+++ RELEASENOTES.md
@@ -0,0 +1,4 @@
+## Release Notes for eslint-config-oraclejet ##
+
+### 3.0.0
+* Initial release
---
+++ es6.js
@@ -0,0 +1,11 @@
+/**
+  Copyright (c) 2015, 2017, Oracle and/or its affiliates.
+  The Universal Permissive License (UPL), Version 1.0
+*/
+// Config for es6
+module.exports = {
+  extends: [
+    './es6/base'
+  ].map(require.resolve),
+  rules: {}
+};
